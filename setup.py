fe7bfd2490d03dfd62a4ff08601628797f1e2b18
t0729 <79731579+t0729@users.noreply.github.com>
2025-10-22 23:15:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent
+
+# 長い説明は GitHub を参照と書く
+long_description = "詳細な説明等は GitHub を参照してください: https://github.com/t0729/codeEEW_parser/"
 
 setup(
     name="codeEEW_parser",
@@ -6,4 +12,7 @@ setup(
     packages=find_packages(),
     author="t0729",
     description="緊急地震速報のコード電文を解析するためのパッケージ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/t0729/codeEEW_parser/",
 )
